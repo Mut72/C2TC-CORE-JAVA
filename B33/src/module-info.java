5eaca48a370eefba9480f98e5b80d5d441251e53
Muthamil <Muthamil@DESKTOP-NR3P8LT>
2022-10-21 13:29:13 +0530

    First Commit

---
+++ B33/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Muthamil
+ *
+ */
+module B33 {
+}
---
+++ B33/src/tns/org/program/Firstprogram.java
@@ -0,0 +1,11 @@
+package tns.org.program;
+
+public class Firstprogram {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		
+
+	}
+
+}
