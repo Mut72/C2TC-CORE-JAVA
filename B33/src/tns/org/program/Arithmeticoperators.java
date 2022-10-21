b1c6f08e0c8966b084ebc54eb62361abebf6e46f
Muthamil <Muthamil@DESKTOP-NR3P8LT>
2022-10-21 13:38:50 +0530

    First Commit

---
+++ B33/src/tns/org/program/Arithmeticoperators.java
@@ -0,0 +1,13 @@
+package tns.org.program;
+
+public class Arithmeticoperators {
+
+	public static void main(String[] args) {
+		int x = 18, y = 28;
+		System.out.println(x+y);
+		System.out.println(--x);
+		System.out.println(++y);
+
+	}
+
+}
