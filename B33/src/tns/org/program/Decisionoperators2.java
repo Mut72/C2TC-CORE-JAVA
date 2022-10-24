9363e28dbc7a8d20cce573a1c9ae488c1de3230c
Muthamil <Muthamil@DESKTOP-NR3P8LT>
2022-10-24 12:49:28 +0530

    First commit

---
+++ B33/src/tns/org/program/Decisionoperators2.java
@@ -0,0 +1,20 @@
+package tns.org.program;
+
+public class Decisionoperators2 {
+
+	public static void main(String[] args) {
+		int x=10,y=8;
+		if(x>=y)
+		{
+			System.out.println("true");
+			
+		}
+		else
+		{
+			System.out.println("false");
+		}
+		 
+
+	}
+
+}
