023e2c358008da28958774a3b111f56fe2d84420
Muthamil <Muthamil@DESKTOP-NR3P8LT>
2023-01-24 21:39:48 +0530

    first commit

---
+++ Firstprojecte/src/Variabledatatype.java
@@ -0,0 +1,15 @@
+
+public class Variabledatatype {
+	public static void main(String[]args) {
+		int number =29;
+		char initial = 'm';
+		float per = 3.0f;
+	
+		System.out.println(number);
+		System.out.println(initial);
+		System.out.println(per);
+		
+		
+	}
+
+}
