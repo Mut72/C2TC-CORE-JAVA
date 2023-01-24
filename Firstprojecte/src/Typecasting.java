8d54a81086496b63ade27337f1c4bfcaadc5d8e8
Muthamil <Muthamil@DESKTOP-NR3P8LT>
2023-01-24 21:38:37 +0530

    first commit

---
+++ Firstprojecte/src/Typecasting.java
@@ -0,0 +1,15 @@
+
+public class Typecasting {
+
+	public static void main(String[] args) {
+		byte b= 5; //1byte
+		int n;  //4byte
+		n=b;    // automated or implicit type conversion
+		
+		       //explicit type convertion is b=(byte)n;
+		
+		System.out.println(n);
+
+	}
+
+}
